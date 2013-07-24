6fde088559546f240ffc6934fa78a8d096b47af5
Caio Gondim <me@caiogondim.com>
2013-07-24 16:51:22 -0300

    Translate code to english
    [Closes #3]

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ window.addEventListener('devicelight', function(event) {
   var iluminacao = event.value
 
   if (iluminacao <= 10) {
-    document.body.classList.add('lampada--is-acesa')
+    document.body.classList.add('lightbulb--is-on')
   } else {
-    document.body.classList.remove('lampada--is-acesa')
+    document.body.classList.remove('lightbulb--is-on')
   }
 })
 
